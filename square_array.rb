c96404f2d941a513d0fa1b98dab3b0ee6ff7a0e7
Naomi-Rhames <naomirhames2020@gmail.com>
2020-11-14 14:58:24 -0600

    commit tree 1a11cd4c7397a616ab5846c4fc06b3132abff1c1

--- square_array.rb
+++ square_array.rb
@@ -2,8 +2,8 @@ require "pry"
 def square_array(numbers) 
   square_array = []
   numbers.each do |number|
-    number * number
     square_array.push number * number
+    binding.pry
   end
   square_array
 end
